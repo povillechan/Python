77ae6cfb82407c9c8130543316ba1d66b7298a22
povillechan <poville@yeah.net>
2018-07-05 11:33:36 +0800

    0705

---
+++ 廖雪峰/错误、调试和测试/单元测试.py
@@ -0,0 +1,38 @@
+# unit test
+class Dict(dict):
+    
+    def __init__(self, **kw):
+        super().__init__(**kw)
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+import unittest
+
+
+class TestDict(unittest.TestCase):
+    def test_init(self):
+        d = Dict(a=1,b='test')
+        self.assertEqual(d.a,1)
+        self.assertEqual(d.b,'test')
+        self.assertTrue(isinstance(d,dict))
+    
+    def test_key(self):
+        d = Dict()
+        d['key'] = 'value'
+        self.assertEqual(d.key, 'value')
+
+    def setUp(self):
+            print('setUp...')
+
+    def tearDown(self):
+        print('tearDown...')
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ 廖雪峰/错误、调试和测试/文档测试.py
@@ -0,0 +1,20 @@
+def fact(n):
+    '''
+    Calculate 1*2*...*n
+
+    >>> fact(1)
+    1
+    >>> fact(10)
+    ?
+    >>> fact(-1)
+    ?
+    '''
+    if n < 1:
+        raise ValueError()
+    if n == 1:
+        return 1
+    return n * fact(n - 1)
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
