8ff126e2890031d95efd11f9124e2012fb9ea5a7
povillechan <poville@yeah.net>
2018-07-30 21:16:51 +0800

    0730

---
+++ 爬虫/hegre-art/hegregirls/massages.py
@@ -0,0 +1,140 @@
+# -*- coding:utf8 -*-
+'''
+Created on 2018年6月1日
+
+@author: chenzf
+'''
+
+import requests
+import re
+from requests.exceptions import RequestException
+from multiprocessing import Pool
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+import json
+
+import os,sys
+parentdir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.insert(0,parentdir)
+from com_tools import utils
+
+utils.dir_path = "d:\\Pictures\\hegre-art\\hegregirls\\massages\\{file_path}"
+
+'''
+parse_page
+
+@author: chenzf
+''' 
+def parse_page(html):
+    image = []  
+
+    soup = BeautifulSoup(html, 'lxml')   
+    #items
+    items  = soup.find_all('div', class_="node-grid")
+    
+    for item in items: 
+        image.append({           
+            'name': item.select_one('.grid-meta h4 a').string,            
+            'board': item.select_one('.content .field-type-image a img').get('src'),
+            'url':  urljoin('http://www.hegregirls.com/', item.select_one('.content .field-type-image a').get('href')),
+            })
+
+    return image
+
+'''
+parse_page
+
+@author: chenzf
+''' 
+def parse_page_detail(html):
+    image = {}
+    soup = BeautifulSoup(html, 'lxml')   
+    #items
+    try:
+        date_release = ""
+        for s in soup.find(class_="title-subtitle").strings:
+            date_release += s
+
+        video = soup.select_one('.mejs-mediaelement video source')
+        if video:
+            video = soup.select_one('.mejs-mediaelement video source').get('src')
+        else:
+            video = None
+        image = {
+            'small':soup.select_one('.field-name-massage-board a img').get('src'),
+            'mid':soup.select_one('.mejs-mediaelement video').get('poster'),
+            'large':soup.select_one('.field-name-massage-board a').get('href'),
+            'video':video,
+            'date':date_release
+        }
+    except:
+        print('error-' + soup.select_one('#page-title').string)
+    return image
+'''
+process_image
+
+@author: chenzf
+'''      
+def process_image(image):
+    dir_name = utils.dir_path.format(file_path=image.get('name'))
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name)     
+    
+    
+    with open(dir_name+'\\info.json', 'w') as f:    
+        json.dump(image, f)
+    
+    
+    for subkeys in ['board']:
+        url = image.get(subkeys)
+        if url:
+             utils.download_file(url, utils.get_file_path(url, image.get('name')+
+                                               '\\'+ subkeys))
+    
+    detail = image.get('detail')
+    if detail:
+        for subkeys in ['small', 'mid', 'large']:
+            url = detail.get(subkeys)
+            if url:
+                utils.download_file(url, utils.get_file_path(url, image.get('name')+
+                                               '\\'+ subkeys))
+
+        video = detail.get('video')
+        if video:
+            utils.download_file(video, utils.get_video_file_path(video, image.get('name')+
+                                               '\\video'))                           
+
+'''
+process_image_detail
+
+@author: chenzf
+'''         
+def process_image_detail(url):
+    detail = None
+    html = utils.get_page_by_chrome(url, '.mejs-poster')
+    if html:
+        detail = parse_page_detail(html)
+      
+    return detail
+     
+'''
+main
+
+@author: chenzf
+'''     
+def main(page):
+    url = 'http://hegregirls.com/massage?page={page}'    
+    html = utils.get_page(url.format(page=page))
+    if html:
+        images = parse_page(html)
+        if images:
+            for image in images:
+                image['detail'] = process_image_detail(image.get('url'))
+                process_image(image)
+
+if __name__ == '__main__':   
+    pool = Pool(3)      
+    pool.map(main,[i  for i in range(0,2)])
+
+    pool.close()
+    pool.join()
--- 爬虫/hegre-art/hegregirls/models.py
+++ 爬虫/hegre-art/hegregirls/models.py
@@ -40,7 +40,7 @@ def parse_page(html):
             if content.select_one('.grid-meta .nick'):
                 nick =  content.select_one('.grid-meta .nick').string
             images.append({
-                    'name': content.find('div', class_='grid-meta').find('a').string,
+                    'name': content.find('div', class_='grid-meta').find('a').string.replace('\"','_').replace(':','_'),
                     'url':  urljoin('http://hegregirls.com/', content.find('div', class_='grid-meta').find('a').attrs['href']),
                     'board': content.find('img').attrs['src'],  
                     'nick': nick,
@@ -104,11 +104,11 @@ def parse_page_detail(html):
                     detail_url = urljoin('http://hegregirls.com/', detail_url)
                     
                 galleries_dict.append({
-                    'name': item.select_one('.grid-meta a').string,
+                    'name': item.select_one('.grid-meta a').string.replace('\"','_').replace(':','_'),
                     'date': date_release,
                     'url': urljoin('http://hegregirls.com/', item.select_one('.field-name-coverl a').attrs['href']),
                     'img':item.find('img').attrs['src'],
-                    'board': item.select_one('.field-name-coverl a').attrs['rel'],
+                    'board': item.select_one('.field-name-coverl a').get('rel')[0],
                     'detail': parse_galleries_detail(detail_url)
                     })  
             except:
@@ -118,12 +118,12 @@ def parse_page_detail(html):
         else:   
             try:   
                 films_dict.append({
-                    'name': item.select_one('.grid-meta a').string,
-                    'url':  urljoin('http://hegregirls.com/', item.select_one('.field-name-movie-cover a').attrs['href']),
+                    'name': item.select_one('.grid-meta a').string.replace('\"','_').replace(':','_'),
                     'img':  item.select_one('.field-name-movie-cover a img').attrs['src'],
-                    'board': item.select_one('a.hegre-poster-zoom').attrs['href'],
+                    'board': item.select_one('a.hegre-poster-zoom').get('href')[0],
                     'url': urljoin('http://hegregirls.com/',item.select_one('.field-name-movie-cover a').attrs['href']),
-                    })        
+                    })       
+                 
             except:
                 print('films_dict dict error')    
 #                 print(item.select_one('.grid-meta a').string)
@@ -181,20 +181,68 @@ def process_image(image):
     with open(dir_name+'\\info.json', 'w') as f:    
         json.dump(image, f)
     
-    
-    for subkeys in ['small','mid','large']:
-        url = image.get(subkeys)
-  #      print(url)
-        if url:
-             utils.download_file(url, utils.get_file_path(url, image.get('name')+
-                                               '\\'+ subkeys))
-    
-    detail = image.get('detail')
-    board = detail.get('board')
+    board = image.get('board')
     if board:
         utils.download_file(board, utils.get_file_path(board, image.get('name')+
                                                '\\board'))  
+    
+#     for subkeys in ['small','mid','large']:
+#         url = image.get(subkeys)
+#   #      print(url)
+#         if url:
+#              utils.download_file(url, utils.get_file_path(url, image.get('name')+
+#                                                '\\'+ subkeys))
+#     
+#     detail = image.get('detail')
+#     board = detail.get('board')
+#     if board:
+#         utils.download_file(board, utils.get_file_path(board, image.get('name')+
+#                                                '\\board'))  
+#             
+    detail = image.get('detail')
+    if detail:
+        board_image = detail.get('board_image')
+        if board_image:
+            utils.download_file(board_image, utils.get_file_path(board_image, image.get('name')+
+                                               '\\board_image'))  
+        poster_image = detail.get('poster_image')
+        if board_image:
+            utils.download_file(poster_image, utils.get_file_path(poster_image, image.get('name')+
+                                               '\\poster_image'))  
             
+        galleries =  detail.get('galleries')
+        if galleries:
+            for gallery in galleries:
+                utils.download_file(gallery.get('img'), utils.get_file_path(gallery.get('img'),  image.get('name')+'\\galleries\\'+gallery.get('name')+
+                                               '\\img'))  
+            
+                utils.download_file(gallery.get('board'), utils.get_file_path(gallery.get('board'), image.get('name')+'\\galleries\\'+gallery.get('name')+
+                                               '\\board'))  
+                
+                detail_s = gallery.get('detail')
+                if detail_s:
+                    if detail_s.get('board_image'):
+                        utils.download_file(detail_s.get('board_image'), utils.get_file_path(detail_s.get('board_image'), image.get('name')+'\\galleries\\'+gallery.get('name')+
+                                               '\\board_image'))  
+                         
+                    if detail_s.get('images'):
+                        for i, val in enumerate(detail_s.get('images')):   
+                            if val.get('small'):
+                                utils.download_file(val.get('small'), utils.get_file_path(val.get('small'), image.get('name')+'\\galleries\\'+gallery.get('name')+'\\'+ str(i) +
+                                               '_small'))  
+                            if val.get('large'):
+                                utils.download_file(val.get('large'), utils.get_file_path(val.get('large'), image.get('name')+'\\galleries\\'+gallery.get('name')+'\\'+ str(i) +
+                                               '_large'))  
+        films =  detail.get('films')
+        if films:
+            for film in films:
+                utils.download_file(film.get('img'), utils.get_file_path(film.get('img'),  image.get('name')+'\\films\\'+film.get('name')+
+                                               '\\img'))  
+            
+                utils.download_file(film.get('board'), utils.get_file_path(film.get('board'), image.get('name')+'\\films\\'+film.get('name')+
+                                               '\\board'))  
+                
+
 def process_image_detail(url):
     detail = None
     html = utils.get_page(url)
@@ -220,12 +268,12 @@ def main(page):
         if images:
             for image in images:
                 image['detail'] = process_image_detail(image.get('url'))
-                print(image)
- #               process_image(image)
+#                 print(image)
+                process_image(image)
 
 if __name__ == '__main__':   
     pool = Pool(3)      
-    pool.map(main,[i  for i in range(0,5)])
+    pool.map(main,[i  for i in range(0,6)])
 
     pool.close()
     pool.join()
