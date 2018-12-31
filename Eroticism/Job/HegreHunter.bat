7db74c03ed16a50484db684d7d00e9ba9aa4ff43
poville <poville@yeah.net>
2019-01-01 00:00:29 +0800

    1231

--- Eroticism/Job/HegreHunter.bat
+++ Eroticism/Job/HegreHunter.bat
@@ -1,5 +1,5 @@
 python ..\Site\HegreHunter\Photos.py -p 4
 
-call cmd /c start python ..\Site\HegreHunter\Photos.py -p 2 -l 20 
+call cmd /c start python ..\Site\HegreHunter\Photos.py -p 2 -l 100
 ping -n 30 127.0.0.1>nul
-call cmd /c start python ..\Site\HegreHunter\Photos.py -p 3 -t 20
+call cmd /c start python ..\Site\HegreHunter\Photos.py -p 3 -t 100
