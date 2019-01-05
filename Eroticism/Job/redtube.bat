8308d01ebca85a7be0e6cdfbea3616eb6537f64b
poville <poville@yeah.net>
2019-01-05 15:27:05 +0800

    0105

--- Eroticism/Job/redtube.bat
+++ Eroticism/Job/redtube.bat
@@ -1,4 +1,4 @@
-python ..\Site\RedTube\videos.py -p 4
-call cmd /c start python ..\Site\RedTube\videos.py -p 2 -l 20
+python ..\Site\RedTube\models.py -p 4
+call cmd /c start python ..\Site\RedTube\models.py -p 2 -l 50
 ping -n 60 127.0.0.1>nul
-call cmd /c start python ..\Site\RedTube\videos.py -p 3 -t 20
+call cmd /c start python ..\Site\RedTube\models.py -p 3 -t 50
