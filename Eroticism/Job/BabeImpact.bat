2cf1016e4da85191e709770641cc1bca8c02d869
poville <poville@yeah.net>
2019-03-11 23:52:46 +0800

    0311

--- Eroticism/Job/BabeImpact.bat
+++ Eroticism/Job/BabeImpact.bat
@@ -1,4 +1,4 @@
-call cmd /c python ..\Site\BabeImpact\Photos.py -p 1
+call cmd /c start python ..\Site\BabeImpact\Photos.py -p 1
 ping -n 30 127.0.0.1>nul
 call cmd /c start python ..\Site\BabeImpact\Photos.py -p 2 -l 100
 ping -n 30 127.0.0.1>nul
--- Eroticism/Job/PleasureGirl.bat
+++ Eroticism/Job/PleasureGirl.bat
@@ -1,4 +1,4 @@
-call cmd /c python ..\Site\PleasureGirl\Photos.py -p 1
+call cmd /c start python ..\Site\PleasureGirl\Photos.py -p 1
 ping -n 30 127.0.0.1>nul
 call cmd /c start python ..\Site\PleasureGirl\Photos.py -p 2 -l 100
 ping -n 30 127.0.0.1>nul
