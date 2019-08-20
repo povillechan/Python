2dfefdae5dcbb5960434883d26d438fab1920eaf
poville <poville@yeah.net>
2019-08-20 13:02:20 +0800

    0820

--- Eroticism/Job/run.bat
+++ Eroticism/Job/run.bat
@@ -1,4 +1,7 @@
 call cmd /c start python ..\site\ExGgirlfriendMarket\Photos.py -p 3 -t 10 -f "L:\Pictures\ExGgirlfriendMarket"
 call cmd /c start python ..\site\GodArtNudes\models.py -p 3 -t 10 -f "L:\Pictures\GodArtNudes"
 call cmd /c start python ..\site\HegreHunter\Photos.py -p 2 -t 10
-call cmd /c start python ..\site\HegreHunter\Photos.py -p 3 -t 10 -f "L:\Pictures\Hunter"
+call cmd /c start python ..\site\SlutsAndAngels\Photos.py -p 3 -t 50 -f "L:\Pictures\SlutsAndAngels"
+call cmd /c start python ..\site\HQSluts\models.py -p 3 -t 50 -f "L:\Pictures\HQSluts"
+call cmd /c start python ..\site\HQBabes\models.py -p 3 -t 50 -f "L:\Pictures\HQBabes"
+call cmd /c start python ..\site\LiveJasminBabes\Photos.py -p 3 -t 50 -f "L:\Pictures\LiveJasminBabes"
